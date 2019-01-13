03c689627db292182f91536b36d6b09ebe0ec464
chenweilong <chenweilong>
2019-01-13 23:04:59 +0800

    no message

---
+++ hyan-starter/src/main/java/com/hyan/hyanstarter/HyanStarterApplication.java
@@ -0,0 +1,14 @@
+package com.hyan.hyanstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyanStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HyanStarterApplication.class, args);
+    }
+
+}
+
